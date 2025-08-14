9333c6b034664f788bcdb17fc904373b796c06fa
fatkhullakh <tfatkh@gmail.com>
2025-08-14 05:24:44 +0200

    Better design for appointment screen: 2 sections: upcoming and past appointments. TODO: Book again feature

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/onboarding/language_screen.dart';
 import 'screens/auth/login_screen.dart';
-import 'screens/customer/customer_home.dart';
 import 'screens/provider/provider_dashboard.dart';
 import 'screens/auth/register_screen.dart';
 import 'screens/auth/change_password_screen.dart';
 import 'screens/test/test_customer_home.dart';
 import 'navigation/nav_root.dart';
-import 'navigation/test_nav_root.dart';
 
 // import 'screens/register_screen.dart';
 
