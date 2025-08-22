298e68693c8dccd7219fd208092a5b794ca638d5
fatkhullakh <tfatkh@gmail.com>
2025-08-22 20:45:56 +0200

    Factoring

--- lib/screens/provider/manage/provider_manage_screen.dart
+++ lib/screens/provider/manage/provider_manage_screen.dart
@@ -4,7 +4,7 @@ import 'services/provider_services_screen.dart';
 import 'business_info/provider_business_info_screen.dart';
 import 'staff/provider_staff_screen.dart';
 import 'hours/provider_business_hours_screen.dart';
-import '../settings/provider_settings_screen.dart';
+import '../manage/business_info/provider_settings_screen.dart';
 
 class ProviderManageScreen extends StatelessWidget {
   final String? providerId; // ← nullable now
